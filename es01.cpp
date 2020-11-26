fcf645e2d9ca6d37afafb24b6a5260650a3b3643
MarinaDuMarezz <73290642+MarinaDuMarezz@users.noreply.github.com>
2020-11-26 19:02:48 +0100

    Add files via upload

---
+++ es01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ( )
+{
+    int x, y, z, p;
+    cout<< "inserire numeri"<<endl;
+    cin>> x;
+    cin>> y;
+    cin>> z;
+    p=x*y*z;
+    cout<< "risultato prodotto "<<p<<endl;
+    return 0;
+}
